5e7a3cbc38ed2d8c762a6024f1ba68148a5370ab
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2022-04-10 23:30:39 +0500

    RegisterViewApi

--- home/views_api.py
+++ home/views_api.py
@@ -39,4 +39,40 @@ class LoginView(APIView):
             print(e)
         return Response(response)
 
-    LoginView = LoginView.as_view()
+LoginView = LoginView.as_view()
+
+class RegisterView(APIView):
+
+        def post(self, request):
+            response = {}
+            response['status'] = 200
+            response['message'] = 'Something went wrong'
+            try:
+                data = request.data
+
+                if data.get('username') is None:
+                    response['message'] = 'key username not found'
+                    raise Exception('key username not found')
+
+                if data.get('password') is None:
+                    response['message'] = 'key password not found'
+                    raise Exception('key password not found')
+
+                check_user = User.objects.filter(username=data.get('username')).first()
+
+                if check_user is None:
+                    response['message'] = 'invalid username, user not found'
+                    raise Exception('invalid username not found')
+
+                user_obj = authenticate(username=data.get('username'), password=data.get('password'))
+
+                if user_obj:
+                    response['status'] = 200
+                    response['message'] = 'Welcome'
+                else:
+                    response['message'] = 'invalid password'
+                    raise Exception('invalid password')
+
+            except Exception as e:
+                print(e)
+            return Response(response)
