abdf1930f762c951fa729cb49e4848f8e7b5ccc2
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2022-04-19 15:37:27 +0500

    main.js

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,8 +1,9 @@
-var csrf = document.getElementById('csrf').value
+console.log("ss")
 
 function login(){
     var username = document.getElementById('loginUsername').value
     var password = document.getElementById('loginPassword').value
+    var csrf = document.getElementById('csrf').value
 
     if(username == '' && password == ''){
         alert('You must enter both')
@@ -17,11 +18,19 @@ function login(){
         method : 'POST',
         headers: {
             'Content-Type' : 'application/json'
+            'X-CSRFToken' : csrf,
         },
-        'X-CSRFToken' : csrf,
-        'body' : JSON.stringify(data)
+
+        body : JSON.stringify(data)
     }).then(result => result.json())
     .then(response => {
+
+        if(response.status == 200){
+            window.location.href = '/'
+        }else{
+            alert(response.message)
+        }
+
         console.log(response)
     })
 }
