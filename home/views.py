7707fedf052d231b719fa88995e88e24a844bd37
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2022-04-20 17:20:36 +0500

    FroalaEditor

--- home/views.py
+++ home/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .form import *
 
 def home(request):
     return render(request, 'home.html')
 
 def login_view(request):
     return render(request, 'login.html')
+
+def add_blog(request):
+    context = {'form' : BlogForm}
+    return render(request, 'add_blog.html', context)
 
 def register_view(request):
     return render(request, 'register.html')
